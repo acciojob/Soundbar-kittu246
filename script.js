92c9aae7df72aaa4d7fa6d02926ff10185816941
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-21 13:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let playButtons = document.getElementsByClassName("btn");
+let stpButton = document.getElementById("stop");
+
+let audioPlay = document.getElementById("audio");
+
+function playMusic (){
+    audioPlay.play();
+}
+
+function stopMusic (){
+    audioPlay.pause();
+}
+
+for(let i=0;i<playButtons.length;i++){
+    playButtons[i].addEventListener("click",playMusic);
+}
+
+stpButton.addEventListener("click",stopMusic);
